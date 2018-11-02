f2ec9aad788b867bf1c4164b0b3f44386c7db8ed
Andrew Hill <andrew.hill@doctors.org.uk>
2018-11-02 01:17:46 +0000

    Initial work to add some stacked area charts on the frailty dashboard.

--- app.R
+++ app.R
@@ -8,42 +8,125 @@
 #
 
 library(shiny)
+library(shinyBS)
+library(dplyr)
+library(ggplot2)
+library(tibbletime)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+source("read_frailty_data.R")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+header <-  dashboardHeader(title = "STHK Frailty Dashboard")
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+## Sidebar content
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Frailty Dashboard"),
+    dashboardSidebar(),
+    dashboardBody(
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+          titlePanel(glue::glue("Frailty service activity (from 
+            {format(min(frailty_data[['Date/Time of Referral']]), format = '%d %b %Y')} to 
+            {format(max(frailty_data[['Date/Time of Referral']]), format = '%d %b %Y')})"))
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        fluidRow(
+            box(title = "Type of residence",
+              width = 10,
+              plotOutput("residence", height = 250)),
+            box(width = 2,
+              radioButtons(inputId = "residenceRadios",
+                           label = "Y axis shows:",
+                           choices = c("Number of referrals",
+                                       "Proportion of referrals")))
+        ),
+        fluidRow(
+          box(title = "Admission mode",
+              width = 10,
+              plotOutput("admission_mode", height = 250))
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+    frailty_data <-
+      read_frailty_data("../FrailtyTest/FrailtyData.csv")
+    
+#    final_output_data <- 
+#      readr::read_csv("../FrailtyTest/FinalTableOutput.csv")
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    frailty_data <- dplyr::group_by(frailty_data,
+                                    .data[["ReferralPeriod"]],
+                                    .data[["Place of Residence"]])
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    residence_type <- dplyr::summarise(frailty_data,
+                                       "Patients" = n())
+    frailty_data <- dplyr::ungroup(frailty_data)
+    frailty_data <- dplyr::group_by(frailty_data,
+                                    .data[["ReferralPeriod"]])
+    patients_per_month <- dplyr::summarise(frailty_data,
+                                           "Total" = n())
+    frailty_data <- dplyr::ungroup(frailty_data)
+    
+    residence_type = dplyr::left_join(residence_type,
+                                      patients_per_month,
+                                      by = "ReferralPeriod")
+    
+    residence_type <- dplyr::mutate(residence_type,
+      "proportionPatients" = .data[["Patients"]] /
+        .data[["Total"]] * 100)
+
+    output$residence <- renderPlot({
+      if (input$residenceRadios == "Number of referrals") {
+        y_axis = "Patients"
+      } else {
+        y_axis = "proportionPatients"
+      }
+      ggplot2::ggplot(residence_type,
+             ggplot2::aes(x = .data[["ReferralPeriod"]],
+                 y = .data[[y_axis]],
+                 fill = .data[["Place of Residence"]])) + 
+        ggplot2::geom_area(alpha = 0.4 , size = 0.2, colour = "black") +
+        ggplot2::labs(xlab = "Month of admission",
+                    ylab = input$residenceRadios)
     })
+
+# Do the same with the referral source (NB needs to be modularised later)
+    
+    frailty_data <- dplyr::group_by(frailty_data,
+                                    .data[["ReferralPeriod"]],
+                                    .data[["Mode of admission"]])
+    
+    admission_mode <- dplyr::summarise(frailty_data,
+                                       "Patients" = n())
+    frailty_data <- dplyr::ungroup(frailty_data)
+
+    admission_mode = dplyr::left_join(admission_mode,
+                                      patients_per_month,
+                                      by = "ReferralPeriod")
+    
+    admission_mode <- dplyr::mutate(admission_mode,
+                                    "proportionPatients" = .data[["Patients"]] /
+                                      .data[["Total"]] * 100)
+    
+    output$admission_mode <- renderPlot({
+      if (input$residenceRadios == "Number of referrals") {
+        y_axis = "Patients"
+      } else {
+        y_axis = "proportionPatients"
+      }
+      ggplot2::ggplot(admission_mode,
+                      ggplot2::aes(x = .data[["ReferralPeriod"]],
+                                   y = .data[[y_axis]],
+                                   fill = .data[["Mode of admission"]])) + 
+        ggplot2::geom_area(alpha = 0.4 , size = 0.2, colour = "black") +
+        ggplot2::labs(xlab = "Month of admission",
+                      ylab = input$residenceRadios)
+    })
+    
+#    my_fun=function(vec){ as.numeric(vec[3]) / sum(data$Value[data$Year==vec[2]]) *100 }
+#    data$prop=apply(data , 1 , my_fun)
+    
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
---
+++ read_frailty_data.R
@@ -0,0 +1,71 @@
+
+read_frailty_data <- function(filename) {
+  frailty_data <- readr::read_csv(
+    file = filename,
+    col_types = readr::cols(
+      .default = readr::col_character(),
+      ID = readr::col_double(),
+      "Hospital number" = readr::col_character(),
+      "Place of Residence" = readr::col_factor(
+        levels = c("Own home",
+                   "Nursing home",
+                   "Sheltered accomodation",
+                   "Residential",
+                   "Respite",
+                   "Supported Living",
+                   "Unknown"),
+        include_na = TRUE),
+      "Mode of admission" = readr::col_factor(
+        levels = c("Case finding",
+                   "Case Finding",
+                   "Direct Admission",
+                   "Direct admission",
+                   "E-Handover",
+                   "Referral",
+                   "Transfer",
+                   "Unknown"),
+        include_na = TRUE),
+        
+      #            "Source of admission" = readr::col_factor(),
+      #            "Mode of admission" = readr::col_factor(),
+      "Date/Time of Referral" = readr::col_datetime(
+        format = "%d/%m/%Y %H:%M:%S"),
+      "Date/Time of Admission to 1A" = readr::col_datetime(
+        format = "%d/%m/%Y %H:%M:%S"),
+      Fall = readr::col_logical(),
+      Cognition = readr::col_logical(),
+      Continence = readr::col_logical(),
+      CMS = readr::col_logical(),
+      Nutrition = readr::col_logical(),
+      MCM = readr::col_logical(),
+      "Bed Manager Override" = readr::col_logical(),
+      "CFS" = readr::col_integer(),
+      "DNA CPR" = readr::col_logical(),
+      "ACP" = readr::col_logical(),
+      "ACP Info" = readr::col_logical()
+    ))
+  
+  frailty_data <- dplyr::mutate(frailty_data,
+    "Date/Time of Referral" = 
+       dplyr::if_else(is.na(.data[["Date/Time of Referral"]]),
+                      .data[["Date/Time of Admission to 1A"]],
+                      .data[["Date/Time of Referral"]]),
+  )
+  
+  frailty_data[["Mode of admission"]][frailty_data[["Mode of admission"]] == "Case Finding"] <- "Case finding"
+  frailty_data[["Mode of admission"]][frailty_data[["Mode of admission"]] == "Direct Admission"] <- "Direct admission"
+  forcats::fct_drop(frailty_data[["Mode of admission"]])
+  
+  frailty_data <- dplyr::filter(frailty_data,
+                                !is.na(.data[["Date/Time of Referral"]]))
+  frailty_data <- tibbletime::as_tbl_time(frailty_data,
+                                          index = "Date/Time of Referral")
+  
+  frailty_data <- dplyr::mutate(
+    frailty_data,
+    ReferralPeriod = tibbletime::collapse_index(
+      as.Date(.data[["Date/Time of Referral"]]),
+      period = "monthly",
+      side = "start",
+      clean = TRUE))
+}

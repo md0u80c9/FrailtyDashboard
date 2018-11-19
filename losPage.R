1c869e544be6f1096bc63e48b104099d2e909964
Andrew Hill <andrew.hill@doctors.org.uk>
2018-11-19 19:08:22 +0000

    Pre-filter length of stay prior to plotting to avoid warnings about unplotted data.

--- losPage.R
+++ losPage.R
@@ -6,17 +6,8 @@ losPageInput <- function(id) {
   
   tagList(
     fluidRow(
-      uiOutput(ns("title"))
-    ),
-    fluidRow(
       column(width = 9,
-          box(title = "Length of stay", width = NULL,
-            plotOutput(ns("chart"), height = 550)
-          ),
-          box( width = NULL, title =
-                "Association between delayed admission and length of stay",
-              plotOutput(ns("admission_delay_on_los"), height = 550)
-          )
+        uiOutput(ns("los_box"))
       ),
       column(width = 3,
         box(width = NULL,
@@ -35,26 +26,49 @@ losPageInput <- function(id) {
 # Module server function
 losPage <- function(input, output, session, source_data) {
   
-  filtered_source_data <- dplyr::filter(source_data,
-                                        !is.na(.data[["LOS"]]))
-  filtered_source_data <- dplyr::select(filtered_source_data,
-                                        "LOS",
-                                        "event",
-                                        "CCG",
-                                        "place_of_residence",
-                                        "mode_of_admission",
-                                        "Date/Time of Referral",
-                                        "source_of_admission",
-                                        "discharge_destination",
-                                        "arrival_to_admission_mins",
-                                        "Hospital number")
-  filtered_source_data <- dplyr::mutate(filtered_source_data,
+  filtered_source_data <- reactive({
+    req(source_data)
+    filtered <- dplyr::filter(source_data,
+      !is.na(.data[["LOS"]]))
+    filtered <- dplyr::select(filtered,
+                              "LOS",
+                              "event",
+                              "CCG",
+                              "place_of_residence",
+                              "mode_of_admission",
+                              "Date/Time of Referral",
+                              "source_of_admission",
+                              "discharge_destination",
+                              "arrival_to_admission_mins",
+                              "Hospital number")
+    dplyr::mutate(filtered,
     "year" = as.factor(format(.data[['Date/Time of Referral']],
                     format = '%Y')))
+  })
+
+  max_LOS_days <- 21
+  
+  max_arrival_delay_hrs <- 72
+  
+  admission_delay_data <- reactive({
+    req(filtered_source_data())
+    delay_data <- dplyr::select(filtered_source_data(),
+                  "arrival_to_admission_mins",
+                  "LOS")
+    dplyr::filter(delay_data,
+      !is.na(.data[["arrival_to_admission_mins"]]),
+      !is.na(.data[["LOS"]]),
+      (.data[["LOS"]] >= 0),
+      (.data[["LOS"]] <= max_LOS_days),
+      (.data[["arrival_to_admission_mins"]] >= 0),
+      (.data[["arrival_to_admission_mins"]] <= max_arrival_delay_hrs),
+    )
+  })
                                         
   los_data <- reactive({
+    req(filtered_source_data(), input$charttype)
     {
-      los_data <- dplyr::mutate(filtered_source_data,
+      los_data <- dplyr::mutate(filtered_source_data(),
         "chart_field" =
           !!sym(charttype_fields[[input$charttype]]))
       if (!is.null(input$lengthofstay_typefilter)) {
@@ -69,7 +83,31 @@ losPage <- function(input, output, session, source_data) {
     return(los_data)
   })
 
+  earliest_to_latest_text <- function(date_column) {
+    if (length(date_column) == 0) {
+      return("")
+    }
+    
+    glue::glue("{earliest} to {latest}",
+      earliest = format(min(date_column), format = '%d %b %Y'),
+      latest = format(max(date_column), format = '%d %b %Y')
+    )
+  }
+  
+  length_of_stay_title <- function(date_column) {
+    min_to_max <- earliest_to_latest_text(date_column)
+    if (min_to_max == "") {
+      "Length of stay"
+    } else {
+      glue::glue("Length of stay (from {min_to_max})")
+    }
+  }
+  
   output$title <- renderUI({
+    los_data <- los_data()
+    browser()
+    earliest_to_latest_text
+    
     titlePanel(glue::glue("Length of stay (from 
           {earliest} to {latest})",
       earliest = format(min(los_data()[['Date/Time of Referral']]),
@@ -96,16 +134,16 @@ losPage <- function(input, output, session, source_data) {
 
   output$filter_controls <- renderUI({
     if (!is.null(input$charttype)) {
-      if (length(levels(filtered_source_data[[
+      if (length(levels(filtered_source_data()[[
         charttype_fields[input$charttype]]])) > 0) {
         box(width = NULL,
           checkboxGroupInput(
           inputId = session$ns("lengthofstay_typefilter"),
           label = "Show",
           choices = levels(
-            filtered_source_data[[charttype_fields[input$charttype]]]),
+            filtered_source_data()[[charttype_fields[input$charttype]]]),
           selected = levels(
-            filtered_source_data[[charttype_fields[input$charttype]]]))
+            filtered_source_data()[[charttype_fields[input$charttype]]]))
           )
         }
       }
@@ -120,44 +158,58 @@ losPage <- function(input, output, session, source_data) {
                        selected = "")
   })
 
-  output$chart_box <- renderUI({
-    box(title = "Length of stay", width = NULL,
-        plotOutput(ns("chart"), height = 550)
+  output$los_box <- renderUI({
+    tagList(
+      box(title = 
+        length_of_stay_title(los_data()[['Date/Time of Referral']]),
+        width = NULL,
+        plotOutput(session$ns("length_of_stay_kaplan_meier"),
+                   height = 550)
+      ),
+      box(width = NULL, title =
+          "Association between delayed admission and length of stay",
+          plotOutput(session$ns("admission_delay_on_los"),
+                     height = 550)
+      )
     )
   })
   
-  output$chart <- renderPlot({
-    fit <- survfit(Surv(LOS, event) ~ chart_field,
-                   data = los_data())
+  output$length_of_stay_kaplan_meier <- renderPlot({
+    if (length(los_data()[['Date/Time of Referral']]) > 0) {
+      fit <- survfit(Surv(LOS, event) ~ chart_field,
+                     data = los_data())
 
-    survminer::ggsurvplot(
-      fit, 
-      data = los_data(),
-      size = 1,                 # change line size
-      conf.int = "Show confidence interval bands" %in% input$lengthofstay_extra,
-      xlim = c(0, 21),          # Limit at 21 days ('super stranded' patients)
-      xlab = "Time from admission in days",   # customize X axis label.
-      break.time.by = 1,     # break X axis in time intervals by 500.
-      ylab = "Proportion of patients remaining in hospital",
-      risk.table = TRUE,        # Add risk table
-      risk.table.title = "Number of patients",
-      risk.table.col = "strata",# Risk table color by groups
-      legend.labs = levels(los_data()[["chart_field"]]),
-      #          c("All"),    # Change legend labels
-      risk.table.height = 0.25, # Useful to change when you have multiple groups
-      ggtheme = theme_bw()      # Change ggplot2 theme
-    )
+      survminer::ggsurvplot(
+        fit, 
+        data = los_data(),
+        size = 1,                 # change line size
+        conf.int = "Show confidence interval bands" %in% input$lengthofstay_extra,
+        xlim = c(0, 21),          # Limit at 21 days ('super stranded' patients)
+        xlab = "Time from admission in days",   # customize X axis label.
+        break.time.by = 1,     # break X axis in time intervals by 500.
+        ylab = "Proportion of patients remaining in hospital",
+        risk.table = TRUE,        # Add risk table
+        risk.table.title = "Number of patients",
+        risk.table.col = "strata",# Risk table color by groups
+        legend.labs = levels(los_data()[["chart_field"]]),
+        #          c("All"),    # Change legend labels
+        risk.table.height = 0.25, # Useful to change when you have multiple groups
+        ggtheme = theme_bw()      # Change ggplot2 theme
+      )
+    } else {
+      geom_blank()
+    }
   })
   
   output$admission_delay_on_los <- renderPlot({
-    ggplot(filtered_source_data, aes(x = arrival_to_admission_mins,
+    ggplot(admission_delay_data(), aes(x = arrival_to_admission_mins,
                                      y = LOS)) + 
       geom_point() +
-      scale_x_continuous(breaks = seq(0, 72,
+      scale_x_continuous(breaks = seq(0, max_arrival_delay_hrs,
                                       by = 6),
-                         limits = c(0, 72)) +
-      scale_y_continuous(breaks = seq(0, 21, by = 1),
-                         limits = c(0, 21)) +
+                         limits = c(0, max_arrival_delay_hrs)) +
+      scale_y_continuous(breaks = seq(0, max_LOS_days, by = 1),
+                         limits = c(0, max_LOS_days)) +
       xlab("Time from arrival to 1A admission (hours)") +
       ylab("Length of stay (days)") +
       geom_smooth(method = lm)
